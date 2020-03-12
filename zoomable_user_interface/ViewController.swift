726a9ff3fbb4e8296b814fdef82e3f9f54e316c2
kwon <skgus2624@gmail.com>
2020-03-12 20:34:53 +0900

    doubletap zoom(zoomout error)

--- zoomable_user_interface/ViewController.swift
+++ zoomable_user_interface/ViewController.swift
@@ -26,11 +26,19 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         tripletap.numberOfTapsRequired = 3
         view.addGestureRecognizer(tripletap)
         
+        let doubletap = UITapGestureRecognizer(target: self, action: #selector(doubleTap))
+        doubletap.numberOfTapsRequired = 2
+        doubletap.numberOfTouchesRequired = 1
+        doubletap.require(toFail: tripletap)
+        doubletap.require(toFail: doubletap)
+        view.addGestureRecognizer(doubletap)
+        
         // two finger double tap
         let tfdoubletap = UITapGestureRecognizer(target: self, action: #selector(twoFingerDoubleTap))
         tfdoubletap.numberOfTapsRequired = 2
         tfdoubletap.numberOfTouchesRequired = 2
         tfdoubletap.require(toFail: tripletap)
+        tfdoubletap.require(toFail: doubletap)
         view.addGestureRecognizer(tfdoubletap)
         
         // tap
@@ -240,9 +248,26 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         return zoomRect
     }
     
+    @objc func doubleTap(_ recognizer: UITapGestureRecognizer) {
+        let scale = min(scrollView.zoomScale * 2, scrollView.maximumZoomScale)
+
+        if scale != scrollView.zoomScale { // zoom in
+            let point = recognizer.location(in: imageView)
+
+            let scrollSize = scrollView.frame.size
+            let size = CGSize(width: scrollSize.width / scrollView.maximumZoomScale,
+                              height: scrollSize.height / scrollView.maximumZoomScale)
+            let origin = CGPoint(x: point.x - size.width / 2,
+                                 y: point.y - size.height / 2)
+            scrollView.zoom(to:CGRect(origin: origin, size: size), animated: true)
+        } else if scrollView.zoomScale == 1 { //zoom out
+            scrollView.zoom(to: zoomRectForScale(scale: scrollView.maximumZoomScale, center: recognizer.location(in: imageView)), animated: true)
+        }
+    }
+    
     @objc func twoFingerDoubleTap() {
         // do something here
-        textfield2.text = "Double tap detected"
+        textfield2.text = "2 finger Double tap detected"
         if scrollView.maximumZoomScale == 2.0{
             scrollView.maximumZoomScale = 1.0
             scrollView.isScrollEnabled = false
