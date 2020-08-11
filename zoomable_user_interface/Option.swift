9f48a80cc3d96fd1bf656b6cba5d824c8b6a2691
kwon <skgus2624@gmail.com>
2020-08-11 12:50:15 +0900

    add dropdown options

--- zoomable_user_interface/Option.swift
+++ zoomable_user_interface/Option.swift
@@ -15,7 +15,15 @@ class Option: UITableViewController {
     
     @IBOutlet weak var option1: UISwitch!
     @IBOutlet weak var option2: UISwitch!
-       
+    @IBOutlet weak var SwipeIsEnabled: UISwitch!
+    @IBOutlet weak var DropdownPID: DropDown!
+    @IBOutlet weak var DropdownCondition: DropDown!
+    @IBOutlet weak var DropdownMode: DropDown!
+    @IBOutlet weak var DropdownImgID: DropDown!
+    @IBOutlet weak var DropdownTarget: DropDown!
+    @IBOutlet weak var DropdownZoomLV: DropDown!
+    @IBOutlet weak var DropdownCenterPoint: DropDown!
+    
     @IBAction func switchAction(_ sender: UISwitch) {
         userDefaults.set(sender.isOn, forKey: "mySwitchValue")
     }
@@ -24,6 +32,10 @@ class Option: UITableViewController {
         userDefaults.set(sender.isOn, forKey: "mySwitchValue2")
     }
 
+    @IBAction func switchAction3(_ sender: UISwitch) {
+        userDefaults.set(sender.isOn, forKey: "SwipeIsEnabled")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,6 +45,11 @@ class Option: UITableViewController {
     
         option1.isOn = userDefaults.bool(forKey: "mySwitchValue")
         option2.isOn = userDefaults.bool(forKey: "mySwitchValue2")
+        SwipeIsEnabled.isOn = userDefaults.bool(forKey: "SwipeIsEnabled")
+        
+         DropdownPID.optionArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        
+         DropdownPID.optionIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         
     }
 }
